c5d3229f362c9d8471a02cbc1e4e5367fd7d5df8
maxdhero1 <maxdhero1@gmail.com>
2026-05-24 19:17:53 +0100

    This is the booking schema

---
+++ Model/bookingModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bookingSchema = new mongoose.Schema({
+    property: {
+        type:mongoose.Schema.type.objectId,
+        ref: 'property',
+        required: true
+    },
+    tenant: {
+        type:mongoose.Schema.type.objectId,
+        ref: 'user',
+        required: true
+    },
+    landlord: {
+        type: mongoose.Schema.type.objectId,
+        ref: 'user',
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+
+    endDate: {
+        type: Date,
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+
+    status:{
+        type: String,
+        enum: ['pending', 'approved', 'completed', 'cancelled', 'rejected'],
+        default: 'pending'
+    },
+
+    paymentStatus: {
+        type: String,
+        enum: ['unpaid', 'paid', 'refunded'],
+        default: 'unpaid'
+    },
+    timeStamp: true
+})
+
+module.exports = mongoose.bookingSchema("booking", bookingSchema)
